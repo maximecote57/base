1adbe53a672a58dec7396bd73234a239fa71637e
Maxime Côté <mcote@reptile.tech>
2017-04-21 17:12:55 -0400

    cleaned up the code

--- src/js/rep.form.js
+++ src/js/rep.form.js
@@ -1,10 +1,12 @@
 ;$(function () {
+
     $('.js-form-input').focus(function () {
         var $field = $(this).closest('.js-form-field');
         $field
             .find('.js-form-label')
             .addClass('is-focus');
     });
+
     $('.js-form-label').click(function () {
         var $field = $(this).closest('.js-form-field');
         $field
